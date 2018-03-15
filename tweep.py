4e9c5db21a95e0c25f4cef61875d3bd826ce80be
Cody Zacharias <cody@live.se>
2018-03-15 19:08:15 -0400

    Rearrange arguments

--- tweep.py
+++ tweep.py
@@ -296,8 +296,9 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser(prog="tweep.py", usage="python3 %(prog)s [options]", description="tweep.py - An Advanced Twitter Scraping Tool")
     ap.add_argument("-u", help="User's Tweets you want to scrape.")
     ap.add_argument("-s", help="Search for Tweets containing this word or phrase.")
-    ap.add_argument("-o", help="Save output to a file.")
     ap.add_argument("-g", help="Search for geocoded tweets.")
+    ap.add_argument("-o", help="Save output to a file.")
+    ap.add_argument("-es", "--elasticsearch", help="Index to Elasticsearch")
     ap.add_argument("--year", help="Filter Tweets before specified year.")
     ap.add_argument("--since", help="Filter Tweets sent since date (Example: 2017-12-27).")
     ap.add_argument("--fruit", help="Display 'low-hanging-fruit' Tweets.", action="store_true")
@@ -310,7 +311,6 @@ if __name__ == "__main__":
     ap.add_argument("--limit", help="Number of Tweets to pull (Increments of 20).")
     ap.add_argument("--count", help="Display number Tweets scraped at the end of session.", action="store_true")
     ap.add_argument("--stats", help="Show number of replies, retweets, and likes", action="store_true")
-    ap.add_argument("-es", "--elasticsearch", help="Index to Elasticsearch")
     arg = ap.parse_args()
 
     check()
