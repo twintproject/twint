97c49148e63d2bd41f269a66d4a0378e63c8dd88
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2018-04-29 05:36:27 -0400

    Create tweet.py

---
+++ twint/tweet.py
@@ -0,0 +1,13 @@
+class Tweet(object):
+	id = ""
+	date = ""
+	datestamp = ""
+	time = ""
+	timestamp = ""
+	timezone = ""
+	username = ""
+	tweet = "" # Text
+	replies = "0"
+	likes = "0"
+	retweets = "0"
+	hashtags = ""
