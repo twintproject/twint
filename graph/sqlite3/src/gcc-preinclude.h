20090b6059e34a2bf14de71571ad586c8b21f1b8
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2018-04-30 01:18:16 -0400

    Delete gcc-preinclude.h

--- graph/sqlite3/src/gcc-preinclude.h
+++
@@ -1,6 +0,0 @@
-
-// http://web.archive.org/web/20140401031018/http://rjpower9000.wordpress.com:80/2012/04/09/fun-with-shared-libraries-version-glibc_2-14-not-found/
-
-#if defined(__linux__) && defined(__x86_64__)
-__asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
-#endif
