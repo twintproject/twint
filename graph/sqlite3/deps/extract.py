13cddae80cb82a085f796adde7e0e6e678feea6e
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2018-04-30 01:17:25 -0400

    Delete extract.py

--- graph/sqlite3/deps/extract.py
+++
@@ -1,9 +0,0 @@
-import sys
-import tarfile
-import os
-
-tarball = os.path.abspath(sys.argv[1])
-dirname = os.path.abspath(sys.argv[2])
-tfile = tarfile.open(tarball,'r:gz');
-tfile.extractall(dirname)
-sys.exit(0)
