cd588d09381a5e85c449bdc61db5b9cd5dffe68a
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2018-05-24 05:08:25 -0400

    Fix "may 14" bug

--- twint/search.py
+++ twint/search.py
@@ -39,7 +39,10 @@ class Search:
 
 	async def Feed(self):
 		connect = aiohttp.TCPConnector(verify_ssl=False)
-		ua = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.139 Chrome/66.0.3359.139 Safari/537.36'}
+		if self.init == -1
+			ua = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.139 Chrome/66.0.3359.139 Safari/537.36'}
+		else:
+			ua = ""
 		async with aiohttp.ClientSession(headers=ua, connector=connect) as session:
 			response = await get.Response(session, await get.Url(self.config, self.init).search())
 		self.feed = []
