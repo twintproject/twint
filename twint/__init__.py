caa02be8717a9ba8ab1f45166173de90974e3efe
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2018-05-26 17:13:23 -0400

    Update __init__.py

--- twint/__init__.py
+++ twint/__init__.py
@@ -1,5 +1,15 @@
+'''
+TWINT - Twitter Intelligence Tool (formerly known as Tweep).
+
+See wiki on Github for in-depth details.
+https://github.com/haccer/twint/wiki
+
+Licensed under MIT License
+Copyright (c) 2018 Cody Zacharias
+'''
 from .followers import Followers
 from .following import Following
 from .favorites import Favorites
+from .profile import Profile
 from .search import Search
 from .config import Config
