0f85bef04541a9f4148c608fc575a10b807e52af
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2018-05-28 17:02:41 -0400

    1.1.3 Update

--- twint/feed.py
+++ twint/feed.py
@@ -1,31 +1,31 @@
 from bs4 import BeautifulSoup
-import json
-import re
+from re import findall
+from json import loads
 
 def Follow(response):
     soup = BeautifulSoup(response, "html.parser")
     follow = soup.find_all("td", "info fifty screenname")
     cursor = soup.find_all("div", "w-button-more")
     try:
-        cursor = re.findall(r'cursor=(.*?)">', str(cursor))[0]
+        cursor = findall(r'cursor=(.*?)">', str(cursor))[0]
     except:
         pass
 
     return follow, cursor
 
-def Favorite(response):
+def Mobile(response):
     soup = BeautifulSoup(response, "html.parser")
     tweets = soup.find_all("span", "metadata")
     max_id = soup.find_all("div", "w-button-more")
     try:
-        max_id = re.findall(r'max_id=(.*?)">', str(max_id))[0]
+        max_id = findall(r'max_id=(.*?)">', str(max_id))[0]
     except:
         pass
 
     return tweets, max_id
 
 def profile(response):
-    json_response = json.loads(response)
+    json_response = loads(response)
     html = json_response["items_html"]
     soup = BeautifulSoup(html, "html.parser")
     feed = soup.find_all("li", "js-stream-item")
@@ -33,7 +33,7 @@ def profile(response):
     return feed, feed[-1]["data-item-id"]
 
 def Json(response):
-    json_response = json.loads(response)
+    json_response = loads(response)
     html = json_response["items_html"]
     soup = BeautifulSoup(html, "html.parser")
     feed = soup.find_all("li", "js-stream-item")
