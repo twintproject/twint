755f3ffc723e2ad628775fba05a85538fa99051d
Francesco Poldi <francesco.poldi@protonmail.ch>
2018-07-28 10:20:25 +0200

    #186 args fix

--- twint/verbose.py
+++ twint/verbose.py
@@ -1,15 +1,15 @@
-def Count(count, username):
+def Count(count, config):
     msg = "[+] Finished: Successfully collected "
     if config.Followers:
-        msg += f"all {count} users who follow @{username}"
+        msg += f"all {count} users who follow @{config.Username}"
     elif config.Following:
-        msg += f"all {count} users who @{username} follows"
+        msg += f"all {count} users who @{config.Username} follows"
     elif config.Favorites:
-        msg += f"{count} Tweets that @{username} liked"
+        msg += f"{count} Tweets that @{config.Username} liked"
     else:
         msg += f"{count} Tweets"
         if config.Username:
-            msg += f" from @{username}"
+            msg += f" from @{config.Username}"
     msg += "."
     print(msg)
 
