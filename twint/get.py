41462277a433c58ddddfc8c535ed286da13ebfb8
Francesco Poldi <francesco.poldi@protonmail.ch>
2018-10-21 23:18:51 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 4c27b2b82cb84e7b8d5fac206b0b8502a5faba4e.

--- twint/get.py
+++ twint/get.py
@@ -10,7 +10,6 @@ from aiohttp_socks import SocksConnector, SocksVer
 
 from . import url
 from .output import Tweets, Users
-from .user import inf
 
 #import logging
 
@@ -102,14 +101,6 @@ async def Username(_id):
     soup = BeautifulSoup(r, "html.parser")
 
     return soup.find("a", "fn url alternate-context")["href"].replace("/", "")
-
-async def UserId(username):
-    #loggin.info("[<] " + str(datetime.now()) + ':: get+UserId')
-    url = f"http://twitter.com/{username}?lang=en"
-    r = await Request(url)
-    soup = BeautifulSoup(r, "html.parser")
-
-    return int(inf(soup, "id"))
 
 async def Tweet(url, config, conn):
     #loggin.info("[<] " + str(datetime.now()) + ':: Tweet')
--- twint/user.py
+++ twint/user.py
@@ -20,10 +20,6 @@ def inf(ur, _type):
         ret = group["data-screen-name"]
     elif _type == "private":
         ret = group["data-protected"]
-        if ret == 'true':
-            ret = 1
-        else:
-            ret = 0
 
     return ret
 
@@ -32,18 +28,18 @@ def card(ur, _type):
         try:
             ret = ur.find("p", "ProfileHeaderCard-bio u-dir").text.replace("\n", " ")
         except:
-            ret = None
+            ret = "None"
     elif _type == "location":
         try:
             ret = ur.find("span", "ProfileHeaderCard-locationText u-dir").text
             ret = ret[15:].replace("\n", " ")[:-10]
         except:
-            ret = None
+            ret = "None"
     elif _type == "url":
         try:
             ret = ur.find("span", "ProfileHeaderCard-urlText u-dir").find("a")["title"]
         except:
-            ret = None
+            ret = "None"
 
     return ret
 
@@ -58,13 +54,11 @@ def convertToInt(x):
         "b" : 1000000000,
     }
     try :
-        if ',' in x:
-            x = x.replace(',', '')
         y = int(x)
         return y
     except :
         pass
-
+    
     try :
         y = float(str(x)[:-1])
         y = y * multDict[str(x)[-1:].lower()]
@@ -85,10 +79,11 @@ def stat(ur, _type):
 
 def media(ur):
     try:
-      media_count = ur.find("a", "PhotoRail-headingWithCount js-nav").text.strip().split(" ")[0]
-      media_count = convertToInt(media_count)
+        media_count = ur.find("a", "PhotoRail-headingWithCount js-nav").text
+        media_count = media_count.replace("\n", "")[32:].split(" ")[0]
+        media_count = convertToInt(media_count)
     except:
-      media_count = 0
+        media_count = "0"
 
     return media_count
 
@@ -96,11 +91,11 @@ def verified(ur):
     try:
         is_verified = ur.find("span", "ProfileHeaderCard-badges").text
         if "Verified account" in is_verified:
-            is_verified = 1
+            is_verified = "true"
         else:
-            is_verified = 0
+            is_verified = "false"
     except:
-        is_verified = 0
+        is_verified = "false"
 
     return is_verified
 
@@ -124,5 +119,4 @@ def User(ur):
     u.is_private = inf(ur, "private")
     u.is_verified = verified(ur)
     u.avatar = ur.find("img", "ProfileAvatar-image")["src"]
-    u.background_image = ur.find('div',{'class':'ProfileCanopy-headerBg'}).find('img').get('src')
     return u
