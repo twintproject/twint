37becb54ebf8a602dc9098bab416f8875ea215c0
Francesco Poldi <francesco.poldi@protonmail.ch>
2018-12-05 14:38:44 +0100

    Fix nonetype error

--- twint/storage/elasticsearch.py
+++ twint/storage/elasticsearch.py
@@ -226,7 +226,13 @@ def Tweet(Tweet, config):
             }
     if config.Near or config.Geo:
         if not _is_near_def:
-            _is_near_def = getLocation(config.Near + config.Geo, near=True)
+            _geo = ""
+            _near = ""
+            if config.Geo:
+                _geo = config.Geo
+            if config.Near:
+                _near = config.Near
+            _is_near_def = getLocation(_near + _geo, near=True)
         if _near:
             j_data["_source"].update({"geo_near": _near})
     if Tweet.place:
