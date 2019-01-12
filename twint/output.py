5c2f57de82a5765dd2394f3973de62cd5144573a
Francesco Poldi <francesco.poldi@protonmail.ch>
2019-01-12 19:53:25 +0100

    Evaluate tweets only if it makes sense #321

--- twint/output.py
+++ twint/output.py
@@ -83,28 +83,30 @@ def _output(obj, output, config, **extra):
 
 async def checkData(tweet, location, config, conn):
     usernames = []
-    user_ids = set()
-    global _duplicate_dict
     copyright = tweet.find("div", "StreamItemContent--withheld")
     if copyright is None and is_tweet(tweet):
         tweet = Tweet(tweet, location, config)
 
-    if datecheck(tweet.datestamp, config):
-        output = format.Tweet(config, tweet)
+        if not tweet.datestamp:
+            print("[x] Hidden tweet found, account suspended due to violation of TOS")
+            return
 
-        if config.Database:
-            db.tweets(conn, tweet, config)
+        if datecheck(tweet.datestamp, config):
+            output = format.Tweet(config, tweet)
 
-        if config.Pandas:
-            panda.update(tweet, config)
+            if config.Database:
+                db.tweets(conn, tweet, config)
 
-        if config.Store_object:
-            tweets_object.append(tweet)
+            if config.Pandas:
+                panda.update(tweet, config)
 
-        if config.Elasticsearch:
-            elasticsearch.Tweet(tweet, config)
+            if config.Store_object:
+                tweets_object.append(tweet)
 
-        _output(tweet, output, config)
+            if config.Elasticsearch:
+                elasticsearch.Tweet(tweet, config)
+
+            _output(tweet, output, config)
 
 async def Tweets(tweets, location, config, conn, url=''):
     if (config.Profile_full or config.Location) and config.Get_replies:
