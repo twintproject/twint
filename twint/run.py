5b5ca6d31515e835490bc48a74c7ebc16894e113
Francesco Poldi <francesco.poldi@protonmail.ch>
2019-06-21 13:20:56 +0200

    Little fix for resume

--- twint/run.py
+++ twint/run.py
@@ -58,6 +58,9 @@ class Twint:
             if self.config.Debug:
                 print(response, file=open("twint-last-request.log", "w", encoding="utf-8"))
 
+            if self.config.Resume:
+                print(self.init, file=open(self.config.Resume, "w", encoding="utf-8"))
+                
             self.feed = []
             try:
                 if self.config.Favorites:
@@ -71,8 +74,6 @@ class Twint:
                         self.feed, self.init = feed.profile(response)
                 elif self.config.TwitterSearch:
                     self.feed, self.init = feed.Json(response)
-                if self.config.Resume:
-                    print(self.init, file=open(self.config.Resume, "w", encoding="utf-8"))
                 break
             except TimeoutError as e:
                 if self.config.Proxy_host.lower() == "tor":
