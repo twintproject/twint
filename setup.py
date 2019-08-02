018ad1e7ec357ef3ba8e68479afbe84e355b9508
Francesco Poldi <francesco.poldi@protonmail.ch>
2019-08-02 13:08:43 +0200

    Added long_description_content_type to setup.py #490

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
 	version=about['__version__'],
 	description=DESCRIPTION,
 	long_description=long_description,
+	long_description_content_type="text/markdown",
 	author=AUTHOR,
 	author_email=EMAIL,
 	python_requires=REQUIRES_PYTHON,
