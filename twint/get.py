91d66781b767e7cd0cdcb74076322ba73df91f39
Francesco Poldi <francesco.poldi@protonmail.ch>
2019-08-02 12:51:41 +0200

    Rised timeout value for get.Response #490

--- twint/get.py
+++ twint/get.py
@@ -158,7 +158,7 @@ async def Request(url, connector=None, params=[], headers=[]):
 
 async def Response(session, url, params=[]):
     logme.debug(__name__+':Response')
-    with timeout(30):
+    with timeout(60):
         async with session.get(url, ssl=False, params=params, proxy=httpproxy) as response:
             return await response.text()
 
