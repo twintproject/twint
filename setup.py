a4b53f994273697699c9b46512b1fcc894c8230d
Aliaksei Urbanski <mim@mim.pw>
2019-09-11 00:46:34 +0300

    Declare support for Python 3.7
    I believe it would be nice to see support for
    modern and upcoming versions of Python.

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: Python',
 		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.6'
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
 		],
 )
