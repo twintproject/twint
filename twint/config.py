681e5b9918c215ae89549da138ee91bd45370ef1
Ishaan Varshney <ishaan.varshney@gmail.com>
2019-12-08 14:22:23 +0100

    :wrench: remove duplicate 'Debug' attribute (#600)

--- twint/config.py
+++ twint/config.py
@@ -43,7 +43,6 @@ class Config:
     Index_tweets = "twinttweets"
     Index_follow = "twintgraph"
     Index_users = "twintuser"
-    Debug = False
     Retries_count = 10
     Resume = None
     Images = False
