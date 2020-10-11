b3556add397e16c51b919e3a050c1365ccfa1762
dirtyDan <44843555+D1rtyDann@users.noreply.github.com>
2020-10-12 01:32:37 +0800

    add RefreshTokenException (#949)
    Co-authored-by: TimsXH <44843555+TimsXH@users.noreply.github.com>

--- twint/token.py
+++ twint/token.py
@@ -9,6 +9,11 @@ class TokenExpiryException(Exception):
     def __init__(self, msg):
         super().__init__(msg)
 
+        
+class RefreshTokenException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+        
 
 class Token:
     def __init__(self, config):
