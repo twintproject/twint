d0e8f0a6e54fdefe3d36319877cceece5c2510f4
Himanshu Dabas <20219940+himanshudabas@users.noreply.github.com>
2020-10-11 20:15:19 +0530

    chore(test): PEP8 formatting

--- test.py
+++ test.py
@@ -5,20 +5,24 @@ import os
 Test.py - Testing TWINT to make sure everything works.
 '''
 
+
 def test_reg(c, run):
     print("[+] Beginning vanilla test in {}".format(str(run)))
     run(c)
+
 
 def test_db(c, run):
     print("[+] Beginning DB test in {}".format(str(run)))
     c.Database = "test_twint.db"
     run(c)
 
+
 def custom(c, run, _type):
     print("[+] Beginning custom {} test in {}".format(_type, str(run)))
     c.Custom['tweet'] = ["id", "username"]
     c.Custom['user'] = ["id", "username"]
     run(c)
+
 
 def test_json(c, run):
     c.Store_json = True
@@ -27,6 +31,7 @@ def test_json(c, run):
     print("[+] Beginning JSON test in {}".format(str(run)))
     run(c)
 
+
 def test_csv(c, run):
     c.Store_csv = True
     c.Output = "test_twint.csv"
@@ -34,32 +39,34 @@ def test_csv(c, run):
     print("[+] Beginning CSV test in {}".format(str(run)))
     run(c)
 
+
 def main():
     c = twint.Config()
     c.Username = "verified"
     c.Limit = 20
     c.Store_object = True
 
-    # Seperate objects are neccessary.
+    # Separate objects are necessary.
 
     f = twint.Config()
     f.Username = "verified"
     f.Limit = 20
     f.Store_object = True
     f.User_full = True
-    
-    runs = [twint.run.Following,
-            twint.run.Followers,
-            twint.run.Search,
-            twint.run.Profile,
-            twint.run.Favorites
-            ]
-    
+
+    runs = [
+        twint.run.Search,  # this works
+        twint.run.Profile,  # this doesn't
+        twint.run.Following,
+        twint.run.Followers,
+        twint.run.Favorites,
+    ]
+
     tests = [test_reg, test_json, test_csv, test_db]
-    
+
     # Something breaks if we don't split these up
 
-    for run in runs[:2]: 
+    for run in runs[:2]:
         for test in tests:
             test(f, run)
 
@@ -70,16 +77,16 @@ def main():
         else:
             c.Since = ""
             c.Until = ""
-        
+
         for test in tests:
             test(c, run)
 
     files = ["test_twint.db", "test_twint.json", "test_twint.csv"]
     for _file in files:
-       os.remove(_file)
-
+        os.remove(_file)
 
     print("[+] Testing complete!")
+
 
 if __name__ == '__main__':
     main()
