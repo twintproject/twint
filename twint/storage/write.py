twintproject/twint

commit ae5e7e1189be1cf319bbd55b921aca6bfb899f8c

Tabbed CSV output (#967)
Twitter does not allow the tab character in tweets, so it is an ideal separator.  Separating with commas (i.e. without this option) results in ambiguity.

Co-authored-by: Friendly <Hacker>

twint/storage/write.py
@@ -53,14 +53,15 @@ def Csv(obj, config):
     fieldnames, row = struct(obj, config.Custom[_obj_type], _obj_type)
     
     base = addExt(config.Output, _obj_type, "csv")
+    dialect = 'excel-tab' if config.Tabs else 'excel'
     
     if not (os.path.exists(base)):
         with open(base, "w", newline='', encoding="utf-8") as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames, dialect=dialect)
             writer.writeheader()
 
     with open(base, "a", newline='', encoding="utf-8") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, dialect=dialect)
         writer.writerow(row)
 
 def Json(obj, config):

twint/cli.py
@@ -87,6 +87,7 @@ def initialize(args):
     c.Phone = args.phone
     c.Verified = args.verified
     c.Store_csv = args.csv
+    c.Tabs = args.tabs
     c.Store_json = args.json
     c.Show_hashtags = args.hashtags
     c.Show_cashtags = args.cashtags
@@ -159,6 +160,7 @@ def options():
     ap.add_argument("--verified", help="Display Tweets only from verified users (Use with -s).",
                     action="store_true")
     ap.add_argument("--csv", help="Write as .csv file.", action="store_true")
+    ap.add_argument("--tabs", help="Separate CSV fields with tab characters, not commas.", action="store_true")
     ap.add_argument("--json", help="Write as .json file", action="store_true")
     ap.add_argument("--hashtags", help="Output hashtags in seperate column.", action="store_true")
     ap.add_argument("--cashtags", help="Output cashtags in seperate column.", action="store_true")

commit 574fa85b2cf7bd2acb400ad2c256a81be1f90405

fixes #976: saving tweets to csv
This patch fixes the issue caused by #967, which broke the functionality of saving the retrieved data into a csv file.

twint/storage/write.py
@@ -53,7 +53,7 @@ def Csv(obj, config):
     fieldnames, row = struct(obj, config.Custom[_obj_type], _obj_type)
     
     base = addExt(config.Output, _obj_type, "csv")
-    dialect = 'excel-tab' if config.Tabs else 'excel'
+    dialect = 'excel-tab' if 'Tabs' in config.__dict__ else 'excel'
     
     if not (os.path.exists(base)):
         with open(base, "w", newline='', encoding="utf-8") as csv_file: