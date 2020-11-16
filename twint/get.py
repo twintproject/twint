aa4893859eee1570b5b1543f6572a6726a2628bc
IBermudes <ilb8@nau.edu>
2020-11-15 17:18:57 -0700

    Fixed file name collision for token.py, changed to twinttoken.py

--- twint/get.py
+++ twint/get.py
@@ -14,7 +14,7 @@ from urllib.parse import quote
 
 from . import url
 from .output import Tweets, Users
-from .token import TokenExpiryException
+from .twinttoken import TokenExpiryException
 
 import logging as logme
 
