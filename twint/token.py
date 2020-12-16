48e8586cb0a49c93a712b86e0824f7db10ec8f35
ajctrl <59609985+ajctrl@users.noreply.github.com>
2020-12-17 08:56:24 +0900

    Update token.py
    fix issue #1061

--- twint/token.py
+++ twint/token.py
@@ -18,6 +18,7 @@ class RefreshTokenException(Exception):
 class Token:
     def __init__(self, config):
         self._session = requests.Session()
+        self._session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'})
         self.config = config
         self._retries = 5
         self._timeout = 10
