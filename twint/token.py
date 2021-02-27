0f8ea9d12e82c4922dd9b75f6957d37fbbec3314
发光的半导体 <694014820@qq.com>
2021-02-28 03:09:52 +0800

    Fix the bug that proxy cannot be used when get token

--- twint/token.py
+++ twint/token.py
@@ -1,50 +1,39 @@
 import re
 import time
-
-import requests
 import logging as logme
-
-
-class TokenExpiryException(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
-
-        
-class RefreshTokenException(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
-        
+import asyncio,aiohttp
+from .get import Request, get_connector
+from .exception import RefreshTokenException
 
 class Token:
     def __init__(self, config):
-        self._session = requests.Session()
-        self._session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'})
+        self._connector = get_connector(config)
+        self._headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'}
         self.config = config
         self._retries = 5
         self._timeout = 10
         self.url = 'https://twitter.com'
 
-    def _request(self):
+    async def _request(self):
         for attempt in range(self._retries + 1):
             # The request is newly prepared on each retry because of potential cookie updates.
-            req = self._session.prepare_request(requests.Request('GET', self.url))
-            logme.debug(f'Retrieving {req.url}')
+            logme.debug(f'Retrieving {self.url}')
             try:
-                r = self._session.send(req, allow_redirects=True, timeout=self._timeout)
-            except requests.exceptions.RequestException as exc:
+                r = await Request(self.url, self._connector, headers=self._headers)
+            except Exception as e:
                 if attempt < self._retries:
                     retrying = ', retrying'
                     level = logme.WARNING
                 else:
                     retrying = ''
                     level = logme.ERROR
-                logme.log(level, f'Error retrieving {req.url}: {exc!r}{retrying}')
+                logme.log(level, f'Error retrieving {self.url}: {e!r}{retrying}')
             else:
                 success, msg = (True, None)
                 msg = f': {msg}' if msg else ''
 
                 if success:
-                    logme.debug(f'{req.url} retrieved successfully{msg}')
+                    logme.debug(f'{self.url} retrieved successfully{msg}')
                     return r
             if attempt < self._retries:
                 # TODO : might wanna tweak this back-off timer
@@ -59,8 +48,9 @@ class Token:
 
     def refresh(self):
         logme.debug('Retrieving guest token')
-        res = self._request()
-        match = re.search(r'\("gt=(\d+);', res.text)
+        loop = asyncio.get_event_loop()
+        text = loop.run_until_complete(self._request())
+        match = re.search(r'\("gt=(\d+);', text)
         if match:
             logme.debug('Found guest token in HTML')
             self.config.Guest_token = str(match.group(1))
