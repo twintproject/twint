ca7bb3afc5923c86e06daffdb337835b02c7b12b
fletchel <70916204+fletchel@users.noreply.github.com>
2021-03-07 18:14:35 +0000

    Update tweet.py

--- twint/tweet.py
+++ twint/tweet.py
@@ -128,6 +128,7 @@ def Tweet(tw, config):
     t.replies_count = tw['reply_count']
     t.retweets_count = tw['retweet_count']
     t.likes_count = tw['favorite_count']
+    t.quotetweet_count = tw['quote_count']
     t.link = f"https://twitter.com/{t.username}/status/{t.id}"
     try:
         if 'user_rt_id' in tw['retweet_data']:
