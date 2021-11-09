a47204658136a2542f44f5d1a3911d39a6f88f50
EJOOSTEROP <49541561+EJOOSTEROP@users.noreply.github.com>
2021-11-09 20:30:39 +0000

    Further building

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import twint
 import pandas
 import flask
 
+from shutil import copyfile
+
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = flask.Flask(__name__)
@@ -37,4 +39,30 @@ if __name__ == "__main__":
     
     app.run(host="localhost", port=8080, debug=True)
 
-    #print(TweetSearch())
+''' 
+Approach:
+- Get List of files and search terms
+-- either hard coded or from config file
+- Copy files from bucket to local
+- Get latest tweet datetime
+- TWINT search with JSON file and 'from' datetime
+- copy files from local to bucket
+'''
+
+def _AppendToFilesJSON():
+
+    fileinfo = {'filepath' : 'tempdata/src/cibc.json', 'search': 'cibc'}
+    files = []
+    files.append(fileinfo)
+
+    for f in files:
+        _CopyFileFromBucket(f['filepath'], '')
+
+
+
+    return 0
+
+def _CopyFileFromBucket(srcfilepath, bucket):
+    dst = 'tempdata/dst/cibc.json'
+    copyfile(srcfilepath, dst)
+    return 0
