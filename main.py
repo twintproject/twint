a1142685df15ecd4beac66e8283396a5ed967989
EJOOSTEROP <49541561+EJOOSTEROP@users.noreply.github.com>
2021-11-10 15:24:56 +0000

    debug config

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ from google.cloud import storage
 from shutil import copyfile
 from datetime import datetime
 import json
+import yaml
 import os
 
 from os import listdir
-
-from .erik import ReadConfigFileGCP
-from .erik import ParseFilesFromConfig 
+ 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -200,6 +199,58 @@ def latest_tweet_in_file(filename_str):
 			latest_tweet_dt = datetime.strptime(tweetsmetad[-1]['created_at'], '%Y-%m-%d %H:%M:%S %Z')
 	#print("...Teets in file before search: ", len(tweetsmetad))
 	return(latest_tweet_dt)
+
+
+#################################################
+#################################################
+## Logic for Configuration file
+#################################################
+#################################################
+def ReadConfigFileGCP():
+    CONFIG_FILE = 'configgcp.yaml'
+
+    #TODO: use GCP credentials; would allow for local testing
+    storage_client = storage.Client()
+    bucketName = 'industrious-eye-330414.appspot.com'
+    bucket = storage_client.get_bucket(bucketName)
+    
+    blob = bucket.blob(CONFIG_FILE)
+    data = blob.download_as_string(client=None)
+
+    configdict = yaml.safe_load(data)
+
+    return configdict
+
+
+def ReadConfigFileLocal():
+    ''' 
+    Returns a dict with the contents of config file
+    '''
+    CONFIG_FILE = 'configgcp.yaml'
+
+    # TODO: Confirm file exists
+    with open(CONFIG_FILE, 'rt') as file:
+        configdict = yaml.safe_load(file.read())
+
+    return configdict
+
+
+
+def ParseFilesFromConfig(configdict):
+    '''
+    Read file information from configgcp file
+    
+    arguments:
+    - configdict: dictionary containing the configfile info
+
+    Returns a list of dictionary values representing files to update with tweets
+    and their search terms. 
+
+    This function is indepenent of location of config file (cloud, local etc.)
+    '''
+    filesinfo = configdict.get('files', ['no files'])
+
+    return filesinfo
 
 if __name__ == "__main__":
     # Used when running locally only. When deploying to Google App
