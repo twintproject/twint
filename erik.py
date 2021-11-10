5a7ab600ad7e4ec22e670685ef1487d494e029b0
EJOOSTEROP <49541561+EJOOSTEROP@users.noreply.github.com>
2021-11-10 22:29:00 +0000

    Basis for SearchEarlierTweets (loading history instead of recent tweets).

--- erik.py
+++ erik.py
@@ -3,9 +3,18 @@ Play area:
 - Create list fo files
 '''
 
+from datetime import datetime, timedelta, timezone
 
+earliest_tweet_dt = datetime.now(timezone.utc)
 
+print(earliest_tweet_dt)
 
+def erik():
+    return 1, 2
+
+result, _ = erik()
+
+print(result)
 
 
 
