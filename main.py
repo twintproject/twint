80d62c5d0e9c24a625e5d8e447863615cf53ab7e
EJOOSTEROP <49541561+EJOOSTEROP@users.noreply.github.com>
2021-11-10 00:32:23 +0000

    debug

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def gcp_AppendToFilesJSON():
 
     result = ""
 
-    myfiles = [f for f in listdir('/tmp')]
+    #myfiles = [f for f in listdir('/tmp')]
     #for f in myfiles:
      #   result = result + '\n' + f
 
