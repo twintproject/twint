f044db0b2b31525277f999830e6933bb9b84f8e8
EJOOSTEROP <49541561+EJOOSTEROP@users.noreply.github.com>
2022-03-30 16:59:21 +0000

    Changing devcontainer.

--- erik.py
+++ erik.py
@@ -17,4 +17,38 @@ result, _ = erik()
 print(result)
 
 
+import twint
+
+def testread():
+    search_str = "transat"
+
+    c = twint.Config()
+	# choose username (optional)
+	#c.Username = "insert username here"
+	# choose search term (optional)
+    c.Search = search_str
+	# choose beginning time (narrow results)
+	#c.Until = str(earliest_tweet_in_file())
+    ##c.Since = str(latest_tweet_in_file(filename_str))
+    # set limit on total tweets
+    c.Limit = 2
+	# no idea, but makes the csv format properly
+	#c.Store_csv = True
+	# format of the csv
+	#c.Custom = ["date", "time", "username", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags"]
+    c.Store_json = True
+	# change the name of the output file
+	##c.Output = filename_str
+	#c.Hide_output = True
+    c.Hide_output = False
+    x = twint.run.Search(c)
+    
+    return(x)
+
+print(testread())
+
+"""I cannot figure out how to get the output just in JSON.
+So save in file first; then 
+"""
+
 
